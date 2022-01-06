5067a88ed978b6adae28b24c33d109e200ab82a3
mint-self <3041271982@qq.com>
2022-01-07 07:21:02 +0800

    first

---
+++ MyBatisTest/mybatis-01/src/main/java/com/xm/mybatis/utils/MybatisUtils.java
@@ -0,0 +1,10 @@
+package com.xm.mybatis.utils;
+import org.apache.ibatis.io.Resources;
+/**
+ * @author mintFM
+ * @create 2021-10-14 17:59
+ */
+public class MybatisUtils {
+    String resource = "mybatis-config.xml";
+
+}
