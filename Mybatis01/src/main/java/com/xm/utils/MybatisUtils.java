4c732e815b853eea3933db52873b14ca1fb3d433
lisi <lisi@qq.com>
2022-03-06 21:09:22 +0800

    mybatis commit

---
+++ Mybatis01/src/main/java/com/xm/utils/MybatisUtils.java
@@ -0,0 +1,17 @@
+package com.xm.utils;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author mintFM
+ * @create 2021-10-16 23:54
+ */
+public class MybatisUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+
+}
