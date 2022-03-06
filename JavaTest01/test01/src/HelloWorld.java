60e848e1351aab5732fd79ab47b11c7d6ad295de
lisi <lisi@qq.com>
2022-03-06 19:37:11 +0800

    commit first

---
+++ JavaTest01/test01/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author mintFM
+ * @create 2021-09-02 8:45
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World！");
+    }
+}
